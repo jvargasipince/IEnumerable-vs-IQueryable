1a65054dcc970eac8ff87f341901404b3f9c4640
jvargasipince <jvargas.ipince@gmail.com>
2017-04-15 17:29:05 -0500

    Add project files.

---
+++ Projection_EF/LoadData_IEnumerable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Projection_EF
+{
+    public class LoadData_IEnumerable
+    {
+        static void Main(string[] args)
+        {
+            GetListEmployees();
+        }
+
+        private static void GetListEmployees()
+        {
+            var AWEntitiesContext = new AdventureWorks2014Entities();
+
+            IEnumerable<Employee> EmployeeList = AWEntitiesContext.Employees.Where(p => p.MaritalStatus.Equals("m", StringComparison.CurrentCultureIgnoreCase));
+            EmployeeList = EmployeeList.Take(10);
+
+            foreach (Employee employee in EmployeeList)
+            {
+                Console.WriteLine("Los datos del empleado son: ");
+                Console.WriteLine("Nro. Documento del empleado : " + employee.NationalIDNumber);
+                Console.WriteLine("Puesto : " + employee.JobTitle);
+                Console.WriteLine("Fecha Nacimiento : " + employee.BirthDate.ToShortDateString());
+                Console.WriteLine("Estado Civil : " + employee.MaritalStatus);
+
+                Console.WriteLine("Pulse una tecla para continuar");
+                Console.ReadKey();
+            }
+
+        }
+
+    }
+}
---
+++ Projection_EF/LoadData_IQueryable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace Projection_EF
+{
+    public class LoadData_IQueryable
+    {
+        static void Main(string[] args)
+        {
+            GetListEmployees();
+        }
+
+
+        private static void GetListEmployees()
+        {
+            var AWEntitiesContext = new AdventureWorks2014Entities();
+
+            IQueryable<Employee> EmployeeList = AWEntitiesContext.Employees.Where(p => p.MaritalStatus.Equals("m", StringComparison.CurrentCultureIgnoreCase));
+            EmployeeList = EmployeeList.Take(10);
+
+            foreach (Employee employee in EmployeeList)
+            {
+                Console.WriteLine("Los datos del empleado son: ");
+                Console.WriteLine("Nro. Documento del empleado : " + employee.NationalIDNumber);
+                Console.WriteLine("Puesto : " + employee.JobTitle);
+                Console.WriteLine("Fecha Nacimiento : " + employee.BirthDate.ToShortDateString());
+                Console.WriteLine("Estado Civil : " + employee.MaritalStatus);
+
+                Console.WriteLine("Pulse una tecla para continuar");
+                Console.ReadKey();
+            }
+
+        }
+
+
+    }
+}
